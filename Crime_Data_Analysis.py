30e197bf578618c5bfe99eca99547df526db6d82
Sumanth Donthula <46747610+sumanthdonthula@users.noreply.github.com>
2024-06-04 02:29:09 -0500

    Add files via upload

---
+++ Crime_Data_Analysis.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Mapple Bluff Crime Data Analysis",
+    page_icon="🚔",
+)
+
+st.image('pandata.jpg',width=100)
+
+
+st.write("# Crime Data Analysis! 🚔👮‍♂️👮‍♀️")
+st.image('mapple_bluff.jpg')
+
+
+st.markdown(
+    """
+    Maple Bluff is a village in Dane County, Wisconsin, United States. The population was 1,313 at the 2010 census. A suburb of Madison, it is part of the Madison Metropolitan Statistical Area. Maple Bluff is the site of the Wisconsin Governor's Mansion, the residence of the governor of Wisconsin and their family.
+    
+    
+    **👈 Checkout the Story for Crime Analysis of Mapple Bluff Vilage** 
+"""
+)
+
+
+
